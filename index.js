0de6a05f2708d428d8ea34f7495f59c82cff7326
sdruzhchenko <sdruzhchenko@mindk.com>
2018-06-22 00:02:38 +0300

    start

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const x = {"xxx" : "xxx"};
+
+// module.exports = require('./src/core');
+module.exports = x;
---
+++ src/core.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const lo = require( 'lodash' );
+
+const runApp = function( res, req, options ){
+
+
+
+};
+
+
+const core = {
+	runApp: runApp,
+};
+
+module.exports = core;
